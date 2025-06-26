604379dd798c1837b9fa8a11ea4322b7cb67827b
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2025-06-26 23:24:40 +0530

    Add files via upload

---
+++ eventhub-system/user-service/src/main/java/com/eventhub/userservice/service/UserService.java
@@ -0,0 +1,26 @@
+package com.eventhub.userservice.service;
+
+import com.eventhub.userservice.model.User;
+import com.eventhub.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+}
